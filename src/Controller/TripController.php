9a8b83ab494526842116ac4cf8acbaffc3e045f0
Antoine-jpg <yanoqua@gmail.com>
2021-10-04 15:09:31 +0200

    TripController

---
+++ src/Controller/TripController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+/**
+ * @Route("/trips", name="app_trips")
+ */
+class TripController
+{
+    /**
+     * @Route("/", name="app_showTrips")
+     */
+    public function showTrips()
+    {
+    }
+
+    /**
+     * @Route("/add", name="app_addTrip")
+     */
+    public function addTrip()
+    {
+    }
+
+    /**
+     * @Route("/search", name="app_searchTrip")
+     */
+    public function searchTrip()
+    {
+    }
+
+    /**
+     * @Route("/show", name="app_showTrip")
+     */
+    public function showTrip()
+    {
+    }
+
+    /**
+     * @Route("/cancel", name="app_cancelTrip")
+     */
+    public function cancelTrip()
+    {
+    }
+
+    /**
+     * @Route("/edit", name="app_editTrip")
+     */
+    public function editTrip()
+    {
+    }
+
+}
