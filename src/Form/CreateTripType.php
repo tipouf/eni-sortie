ea904c1f1b0da9778107114e6595092eb162ad57
kevin-huet <kevin.huet@epitech.eu>
2021-10-05 15:39:50 +0200

    add create trip form

--- src/Form/CreateTripType.php
+++ src/Form/CreateTripType.php
@@ -2,8 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
+use App\Entity\Location;
 use App\Entity\Trip;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,24 +20,34 @@ class CreateTripType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('startedAt')
-            ->add('duration')
-            ->add('registrationLimit')
-            ->add('registrationNumber')
-            ->add('description')
-            ->add('promoter')
-            ->add('promoterContributor')
-            ->add('location')
-            ->add('status')
-            ->add('contributors')
+            ->add('name', TextType::class)
+            ->add('startedAt', DateTimeType::class)
+            ->add('duration', NumberType::class)
+            ->add('registrationLimit', NumberType::class)
+            ->add('description', TextareaType::class)
+            ->add('promoter', EntityType::class, [
+                'class' => Campus::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.name', 'ASC');
+                },
+                'choice_label' => 'name',
+            ])
+            ->add('location', EntityType::class, [
+                'class' => Location::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->orderBy('u.name', 'ASC');
+                },
+                'choice_label' => 'name',
+            ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Trip::class,
+            'data_class' => null,
         ]);
     }
 }
