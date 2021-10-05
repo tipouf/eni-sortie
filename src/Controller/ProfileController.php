8f5c966656241effac1ff52a2a65667bf42e676a
nicolas cindon <nicolas.cindon2020@campus-eni.fr>
2021-10-05 17:33:06 +0200

    ongoing profil and modif css

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -25,5 +25,24 @@ class ProfileController extends AbstractController
       'contributor' => $contributor,
     ]);
   }
+  /**
+   * @Route("/{contributor}/edit", name="contributor_edit", methods={"GET", "POST"})
+   */
+  public function edit(Request $request, Contributor $contributor): Response
+  {
+    $form = $this->createForm(EditContributorType::class, $contributor);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $this->getDoctrine()->getManager()->flush();
+
+      return $this->redirectToRoute('profile/profile.html.twig', [  'contributor' => $contributor], Response::HTTP_SEE_OTHER);
+    }
+
+    return $this->renderForm('profile/edit.html.twig', [
+      'contributor' => $contributor,
+      'form' => $form,
+    ]);
+  }
 
 }
