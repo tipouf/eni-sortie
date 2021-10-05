42cfaf468c050937b38be1bcb8b464397c021300
nicolas cindon <nicolas.cindon2020@campus-eni.fr>
2021-10-05 12:18:51 +0200

    city crud ongoing

--- src/Controller/CityController.php
+++ src/Controller/CityController.php
@@ -4,10 +4,11 @@ namespace App\Controller;
 
 use App\Form\CityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CityRepository;
-
+use App\Entity\City;
 
 /**
  * @Route("/cities")
@@ -15,19 +16,18 @@ use App\Repository\CityRepository;
 class CityController extends AbstractController
 {
   /**
-   * @Route("/", name="main_cities", methods={"GET"})
+   * @Route("/", name="main_cities")
    */
   public function main(CityRepository $cityRepository):Response
   {
-    return $this->render('city/cities.html.twig', [
+    return $this->render("city/cities.html.twig",[
       'cities' => $cityRepository->findAll(),
     ]);
   }
-
   /**
    * @Route("/edit/{city}", name="edit_city", methods={"GET","POST"})
    */
-  public function editCity():Response
+  public function editCity(Request $request, City $city):Response
   {
     $form = $this->createForm(CityType::class, $city);
     $form->handleRequest($request);
@@ -35,7 +35,7 @@ class CityController extends AbstractController
     if ($form->isSubmitted() && $form->isValid()) {
       $this->getDoctrine()->getManager()->flush();
 
-      return $this->redirectToRoute('city_index', [], Response::HTTP_SEE_OTHER);
+      return $this->redirectToRoute('main_cities', [], Response::HTTP_SEE_OTHER);
     }
 
     return $this->renderForm('city/edit_city.html.twig', [
@@ -53,18 +53,39 @@ class CityController extends AbstractController
   }
 
   /**
-   * @Route("/delete/{city}", name="delete_city", methods={"POST"})
+   * @Route("/delete/{city}", name="delete_city", methods={"GET","POST"})
    */
-  public function deleteCity():Response
+  public function deleteCity(Request $request, City $city):Response
   {
-    return response('DELETE', 200);
+    if ($this->isCsrfTokenValid('delete'.$city->getId(), $request->request->get('_token'))) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->remove($city);
+      $entityManager->flush();
+    }
+
+    return $this->redirectToRoute('main_cities', [], Response::HTTP_SEE_OTHER);
   }
 
   /**
-   * @Route("/add", name="add_city", methods={"POST"})
+   * @Route("/add", name="add_city", methods={"GET", "POST"})
    */
-  public function addCity():Response
+  public function addCity(Request $request):Response
   {
-    return $this->render('city/add_city.html.twig');
+    $city = new City();
+    $form = $this->createForm(CityType::class, $city);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($city);
+      $entityManager->flush();
+
+      return $this->redirectToRoute('main_cities', [], Response::HTTP_SEE_OTHER);
+    }
+
+    return $this->renderForm('city/add_city.html.twig', [
+      'city' => $city,
+      'form' => $form,
+    ]);
   }
 }
