e08686de53e50dcac0f098eeab1bf520945bb34f
Antoine-jpg <yanoqua@gmail.com>
2021-10-08 16:55:45 +0200

    merge

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Status;
 use App\Entity\Trip;
+use App\Form\CancelTripType;
 use App\Form\CreateTripType;
-use App\Form\FilterType;
-use App\Model\FilterModel;
+
 use App\Model\TripModel;
 use App\Services\TripService;
 
@@ -22,115 +23,107 @@ use Doctrine\ORM\EntityManagerInterface;
  */
 class TripController extends AbstractController
 {
-  private TripService $tripService;
-  private EntityManagerInterface $manager;
-  private TripRepository $tripRepository;
+    private TripService $tripService;
+    private EntityManagerInterface $manager;
+    private TripRepository $tripRepository;
 
-  public function __construct(TripService $tripService, EntityManagerInterface $manager, TripRepository $tripRepository)
-  {
-    $this->tripService = $tripService;
-    $this->manager = $manager;
-    $this->tripRepository = $tripRepository;
-  }
-
-  /**
-   * @Route("/", name="app_showTrips")
-   */
-  public function showTrips(Request $request)
-  {
-    $model = new FilterModel();
-    $trips = $this->tripService->getAllTrips();
-    $form = $this->createForm(FilterType::class, $model);
-
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-      $trips = $this->tripService->getByFilters($model);
+    public function __construct(TripService $tripService, EntityManagerInterface $manager, TripRepository $tripRepository)
+    {
+        $this->tripService = $tripService;
+        $this->manager = $manager;
+        $this->tripRepository = $tripRepository;
     }
 
-    return $this->render('trip/trips_list.html.twig', [
-      'trips' => $trips,
-      'form' => $form->createView()
-    ]);
-  }
+    /**
+     * @Route("/", name="app_showTrips")
+     */
+    public function showTrips(Request $request)
+    {
+        $model = new FilterModel();
+        $trips = $this->tripService->getAllTrips();
+        $form = $this->createForm(FilterType::class, $model);
 
-  /**
-   * @Route("/add", name="app_addTrip")
-   */
-  public function addTrip(Request $request)
-  {
-    $model = new TripModel();
-    $form = $this->createForm(CreateTripType::class, $model);
-    $form->handleRequest($request);
-    if ($form->isSubmitted() && $form->isValid()) {
-      $this->tripService->createTrip($model);
+        $form->handleRequest($request);
 
-      return $this->redirectToRoute('app_home');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $trips = $this->tripService->getByFilters($model);
+        }
+
+        return $this->render('trip/trips_list.html.twig', [
+            'trips' => $trips,
+            'form' => $form->createView()
+        ]);
     }
-    return $this->render('trip/new_trip.html.twig', [
-      'form' => $form->createView()
-    ]);
-  }
 
-  /**
-   * @Route("/search", name="app_searchTrip")
-   */
-  public function searchTrip()
-  {
-  }
+    /**
+     * @Route("/add", name="app_addTrip")
+     */
+    public function addTrip(Request $request)
+    {
+        $model = new TripModel();
+        $form = $this->createForm(CreateTripType::class, $model);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->tripService->createTrip($model);
 
-  /**
-   * @Route("/{trip}", name="app_showTrip", methods={"GET"})
-   */
-  public function showTrip(Trip $trip): Response
-  {
-    return $this->render('trip/show_trip.html.twig', [
-      'trip' => $trip,
-    ]);
-  }
-
-  /**
-   * @Route("/cancel", name="app_cancelTrip")
-   */
-  public function cancelTrip()
-  {
-  }
-
-  /**
-   * @Route("/{trip}/edit", name="app_editTrip")
-   */
-  public function editTrip(Trip $trip, Request $request)
-  {
-    $model = new TripModel();
-    $form = $this->createForm(CreateTripType::class, $model);
-    $form->handleRequest($request);
-    if ($form->isSubmitted() && $form->isValid()) {
-      $this->tripService->createTrip($model, $trip);
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('trip/new_trip.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
-    return $this->render('trip/edit_trip.html.twig', [
-      'form' => $form->createView(),
-      'trip' => $trip
-    ]);
-  }
 
-  /**
-   * @Route("/{trip}/subscribe", name="app_subscribeTrip")
-   */
-  public function subscribeTrip(Trip $trip)
-  {
-    $contributor = $this->getUser();
-    $this->tripService->subscribeTrip($trip, $contributor);
-    return $this->redirectToRoute('app_showTrips');
-  }
+    /**
+     * @Route("/search", name="app_searchTrip")
+     */
+    public function searchTrip()
+    {
+    }
 
-  /**
-   * @Route("/{trip}/unsubscribe", name="app_unsubscribeTrip")
-   */
-  public function unsubscribeTrip(Trip $trip)
-  {
-    $contributor = $this->getUser();
-    $this->tripService->unsubscribeTrip($trip, $contributor);
-    return $this->redirectToRoute('app_showTrips');
 
-  }
+
+    /**
+     * @Route("/{trip}", name="app_showTrip", methods={"GET"})
+     */
+    public function showTrip(Trip $trip): Response
+    {
+        return $this->render('trip/show_trip.html.twig', [
+            'trip' => $trip,
+        ]);
+    }
+
+    /**
+     * @Route("/{trip}/cancel", name="app_cancelTrip")
+     */
+    public function cancelTrip(Request $request, Trip $trip): Response
+    {
+        $form = $this->createForm(CancelTripType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $motif = $form->get("motif")->getData();
+            $this->tripService->changeStatus($trip, Status::CANCELED);
+        }
+
+        return $this->render('trip/cancel_trip.html.twig', [
+            'trip' => $trip,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/{trip}/edit", name="app_editTrip")
+     */
+    public function editTrip(Trip $trip, Request $request)
+    {
+        $model = new TripModel();
+        $form = $this->createForm(CreateTripType::class, $model);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->tripService->createTrip($model, $trip);
+        }
+        return $this->render('trip/edit_trip.html.twig', [
+            'form' => $form->createView(),
+            'trip' => $trip
+        ]);
+    }
 }
