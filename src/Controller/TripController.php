d4e08e5809c7722746934acf07e7ae64a64029ce
kevin-huet <kevin.huet@epitech.eu>
2021-10-08 17:36:07 +0200

    merge

---
+++ src/Controller/TripController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trip;
+use App\Form\CreateTripType;
+use App\Form\FilterType;
+use App\Model\FilterModel;
+use App\Model\TripModel;
+use App\Services\TripService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/trips")
+ */
+class TripController extends AbstractController
+{
+    private TripService $tripService;
+
+    public function __construct(TripService $tripService)
+    {
+        $this->tripService = $tripService;
+    }
+
+    /**
+     * @Route("/", name="app_showTrips")
+     */
+    public function showTrips(Request $request)
+    {
+        $model = new FilterModel();
+        $trips = $this->tripService->getAllTrips();
+        $form = $this->createForm(FilterType::class, $model);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $trips = $this->tripService->getByFilters($model);
+        }
+
+        return $this->render('trip/trips_list.html.twig', [
+            'trips' => $trips,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="app_addTrip")
+     */
+    public function addTrip(Request $request)
+    {
+        $model = new Trip();
+        $form = $this->createForm(CreateTripType::class, $model);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //
+            $newLocation = $form->get('locationType')->getData();
+            $city = $form->get('city')->getData();
+            $this->tripService->createTrip($model, $city, $newLocation);
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('trip/new_trip.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="app_searchTrip")
+     */
+    public function searchTrip()
+    {
+    }
+
+    /**
+     * @Route("/{trip}", name="app_showTrip", methods={"GET"})
+     */
+    public function showTrip(Trip $trip): Response
+    {
+        return $this->render('trip/show_trip.html.twig', [
+            'trip' => $trip,
+        ]);
+    }
+
+    /**
+     * @Route("/cancel", name="app_cancelTrip")
+     */
+    public function cancelTrip()
+    {
+    }
+
+    /**
+     * @Route("/{trip}/edit", name="app_editTrip")
+     */
+    public function editTrip(Trip $trip, Request $request)
+    {
+        $form = $this->createForm(CreateTripType::class, $trip);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //
+            $newLocation = $form->get('locationType')->getData();
+            $city = $form->get('city')->getData();
+            $this->tripService->createTrip($trip, $city, $newLocation);
+        }
+        return $this->render('trip/edit_trip.html.twig', [
+            'form' => $form->createView(),
+            'trip' => $trip
+        ]);
+    }
+
+    /**
+     * @Route("/{trip}/subscribe", name="app_subscribeTrip")
+     */
+    public function subscribeTrip(Trip $trip)
+    {
+        $contributor = $this->getUser();
+        $this->tripService->subscribeTrip($trip, $contributor);
+        return $this->redirectToRoute('app_showTrips');
+    }
+
+    /**
+     * @Route("/{trip}/unsubscribe", name="app_unsubscribeTrip")
+     */
+    public function unsubscribeTrip(Trip $trip)
+    {
+        $contributor = $this->getUser();
+        $this->tripService->unsubscribeTrip($trip, $contributor);
+        return $this->redirectToRoute('app_showTrips');
+
+    }
+}
---
+++ src/Services/TripService.php
@@ -0,0 +1,95 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Entity\City;
+use App\Entity\Location;
+use App\Entity\Status;
+use App\Entity\Trip;
+use App\Model\FilterModel;
+use App\Model\LocationModel;
+use App\Model\TripModel;
+use App\Repository\LocationRepository;
+use App\Repository\StatusRepository;
+use App\Repository\TripRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+
+class TripService
+{
+    private TripRepository $tripRepository;
+    private EntityManagerInterface $em;
+    private StatusRepository $statusRepository;
+    private Security $security;
+    private LocationRepository $locationRepository;
+
+    public function __construct(EntityManagerInterface $em, TripRepository $tripRepository, StatusRepository $statusRepository,
+                                LocationRepository $locationRepository, Security $security)
+    {
+        $this->security = $security;
+        $this->statusRepository = $statusRepository;
+        $this->tripRepository = $tripRepository;
+        $this->locationRepository = $locationRepository;
+        $this->em = $em;
+    }
+
+    public function createTrip(Trip $trip, City $city = null, LocationModel $location = null)
+    {
+        if ($location) {
+            $newLocation = new Location($location->getName(), $location->getStreet(),
+                $location->getLatitude(), $location->getLongitude(), $city);
+            $city->addLocation($newLocation);
+            $newLocation->addTrip($trip);
+            $this->em->persist($newLocation);
+            $this->em->persist($city);
+            $trip->setLocation($newLocation);
+        }
+        $trip->setPromoterContributor($this->security->getUser());
+        $this->changeStatus($trip, Status::CREATED);
+        $this->em->persist($trip);
+
+        $this->em->flush();
+    }
+
+    public function addTrip(Trip $trip)
+    {
+        $this->em->persist($trip);
+        $this->em->flush();
+    }
+
+    public function getTripsByDate(\DateTime $dateTime): array
+    {
+        return $this->tripRepository->findBy(["startedAt" => $dateTime]);
+    }
+
+    public function getAllTrips(): array
+    {
+        return $this->tripRepository->findAll();
+    }
+
+    public function getTripsByStatus(Status $status): array
+    {
+        return $this->tripRepository->findBy(["status" => $status]);
+    }
+
+    public function getTripsByStatusName(string $status)
+    {
+        return $this->tripRepository->findByStatusName($status);
+    }
+
+    public function changeStatus(Trip $trip, string $status)
+    {
+        $status = $this->statusRepository->findOneBy(["label" => $status]);
+        if (!$status)
+            return;
+        $trip->setStatus($status);
+        $this->em->persist($trip);
+        $this->em->flush();
+    }
+
+    public function getByFilters(FilterModel $model) {
+        return $this->tripRepository->findByFilters($model, $this->security->getUser());
+    }
+}
